3160a24f793c3d8030bb173e984a7cbb74c282be
Petrovskyi <ivan.prog74@gmail.com>
2026-09-12 10:26:43 +0200

    Fix merge conflict markers for deploy

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import js from '@eslint/js';
 import globals from 'globals';
 import { defineConfig } from 'eslint/config';
@@ -16,23 +15,3 @@ export default defineConfig([
     },
   },
 ]);
-=======
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
-
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
-
-export default eslintConfig;
->>>>>>> origin/backend
